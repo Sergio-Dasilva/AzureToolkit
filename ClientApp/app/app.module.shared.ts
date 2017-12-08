ccba89d22e998f9c92a97b85c248186926558b63
Sergio Dasilva <sergio_dasilva@rogers.com>
2017-12-07 23:01:51 -0500

    Adding Azure Cognative Search API

--- ClientApp/app/app.module.shared.ts
+++ ClientApp/app/app.module.shared.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+//import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
@@ -10,6 +10,7 @@ import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
 import { SearchComponent } from './components/search/search.component';
+import { CommonModule } from './common/common.module';
 
 @NgModule({
     declarations: [
@@ -26,11 +27,11 @@ import { SearchComponent } from './components/search/search.component';
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
-         //   { path: 'counter', component: CounterComponent },
             { path: 'search', component: SearchComponent },
             { path: 'fetch-data', component: FetchDataComponent },
             { path: '**', redirectTo: 'home' }
-        ])
+        ]),
+        CommonModule
     ]
 })
 export class AppModuleShared {
